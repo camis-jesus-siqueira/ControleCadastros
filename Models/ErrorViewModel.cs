ac47f6c16991da2dd1258c15188eb27842285ff4
Giovanna Alves de Farias <giovannaalves02@hotmail.com>
2022-11-24 12:25:32 -0300

    ;sldkf;sdkfl

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ControleCadastros.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
